be3ac34dfd77751e2b55df9e40fef2dde1c9c6dc
rushi <rushendrakumar12@gmail.com>
2023-07-27 19:26:57 +0800

    third commit

---
+++ run - Copy.bat
@@ -0,0 +1,10 @@
+@echo
+pytest -v -s test_LoginPage.py
+
+REM pytest -v -s test_LoginPage.py
+
+REM pytest -v -s test_LoginPage.py
+
+REM pytest -v -s test_LoginPage.py
+
+pause
---
+++ testCases/run.bat
@@ -0,0 +1,4 @@
+@echo
+pytest -v -s C:\Users\Sindhu\PycharmProjects\quickerAutomation\testCases\test_LoginPage.py 
+
+pause
